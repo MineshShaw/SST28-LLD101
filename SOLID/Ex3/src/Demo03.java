36ff26064ee8f4d712435a428f0a39f4d3c147fa
MineshShaw <mineshshaw2007@gmail.com>
2026-02-22 14:03:41 +0530

    Completed Ex1-6

--- SOLID/Ex3/src/Demo03.java
+++ SOLID/Ex3/src/Demo03.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class Main {
+public class Demo03 {
     public static void main(String[] args) {
         System.out.println("=== Placement Eligibility ===");
         RuleInput ruleInput = new RuleInput();
